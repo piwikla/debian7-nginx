8f73dae8369a383c5cd67947162842003b9feecb
piwik-la <admin@piwik.la>
2014-04-13 00:45:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,6 +56,5 @@ apt-get install fail2ban iptables-persistent
 #Remove Apache2
 apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker  
 
-
 #Start Nginx
 service nginx start
